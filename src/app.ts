42c52a3a90b8bec250df4673f83956f1e30b1df9
Seungwon Jung <124873708+jswon-jung@users.noreply.github.com>
2023-10-17 20:58:39 +0900

    Docs: Cors 설정 수정

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 
-app.use(cors({ origin: '*', credentials: true }));
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
