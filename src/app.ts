8b3f61afb5afa4bd6b1f4eaaa1e74980d70f9ab2
Seungwon Jung <124873708+jswon-jung@users.noreply.github.com>
2023-11-30 18:11:42 +0900

    Test: 프론트엔드 배포 cors 설정

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,7 @@ app.use(
         origin: [
             'http://localhost:5173',
             'https://netlify.com/',
+            'https://quiet-chebakia-eb017d.netlify.app/'
         ],
         credentials: true,
     }),
